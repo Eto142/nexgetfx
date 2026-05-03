50e88a2215130c9e1f4628c9a6de5c42ca18b061
eto <eto.saviour80@gmail.com>
2026-05-03 23:42:38 +0100

    update

--- public/clear.php
+++
@@ -1,5 +0,0 @@
-<?php
-$base = dirname(__DIR__);
-$output = shell_exec('cd ' . escapeshellarg($base) . ' && php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:clear 2>&1');
-echo '<pre>' . htmlspecialchars($output) . '</pre>';
-echo '<br><strong>Done! Delete this file now.</strong>';
